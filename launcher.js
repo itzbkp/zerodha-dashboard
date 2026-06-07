619cfa29ac8d1231d1e5d45e64746f6235e4ec94
itzbkp <mr.n.bkp@gmail.com>
2026-06-07 14:19:08 +0530

    Added launcher.js for local execution
    Signed-off-by: itzbkp <mr.n.bkp@gmail.com>

---
+++ launcher.js
@@ -0,0 +1,8 @@
+const server = require("./server");
+require("dotenv").config({ quiet: true });
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
